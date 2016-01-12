773b09ce11ba5e9c0f3b8d9667cfdd00e64dc6ae
Lucas Burg <lucasburgmota@gmail.com>
2016-01-12 14:35:22 -0200

    Ajuste na sintax

--- src/modules/auth/autentication.js
+++ src/modules/auth/autentication.js
@@ -15,13 +15,7 @@ const autentication = (req, res, next) => {
         } else if (rows[0] !== undefined && rows[0].id !== undefined && rows[0].senha !== undefined) {
             bcrypt.compare(req.body.password, rows[0].senha, (err, result) => {
                 if (result) {
-                    
-                    
-                    
                     res.locals.id = rows[0].id;
-                    
-                    
-                    
                     next();
                 } else {
                     res.sendStatus(401);
